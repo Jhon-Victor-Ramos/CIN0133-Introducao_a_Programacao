7fbd46f1dca65eae8d6085a9c2c77e9953092ef4
Jhon Victor <jhon.vrmartins@gmail.com>
2026-08-17 00:59:32 -0300

    feat: resolve problema 6 (Nickname)

---
+++ problems/6 - Nickname.py
@@ -0,0 +1,12 @@
+"""
+    Esse nickname precisa atender as seguintes restrições:
+    1. Mínimo de 3 caracteres;
+    2. Máximo de 16 caracteres;
+    3. Apenas caracteres alfanuméricos e underlines são permitidos.
+    4. NÃO pode usar espaço
+"""
+
+first_name = str(input())
+last_name = str(input())
+
+print(first_name+last_name)
