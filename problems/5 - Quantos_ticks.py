9b724fec3d299bfc709a61c870a341adc042a7ec
Jhon Victor <jhon.vrmartins@gmail.com>
2026-08-17 14:55:44 -0300

    docs: correção dos comentários
    exclusão da linha 4 antiga que continha apenas um espaço vazio e correção de $6 \rightarrow 3$ no comentário da linha `10` onde explica o por quê da multiplicação de $12000 \times 3$

--- problems/5 - Quantos_ticks.py
+++ problems/5 - Quantos_ticks.py
@@ -1,14 +1,13 @@
 """
     3h por dia
     12000 ticks que Tantan passou construindo (ciclo diurno, 10min na vida real)
-
 """
 
 
 real_life_days = int(input())
 houses_amount = int(input())
 
-total_ticks_in_hours = 12000 * 3 # como 12000 são 10min, então multiplica por 6 para pegar quantos ticks jogáveis ele passou em uma hora
+total_ticks_in_hours = 12000 * 3 # como 12000 são 10min, então multiplica por 3 para pegar quantos ticks jogáveis ele passou em uma hora
 
 total_time_in_hours = real_life_days * 3
 
